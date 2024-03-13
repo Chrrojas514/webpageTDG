e6e81f315e1b86bb838fb97e22d1a2797f8b2067
unknown <chrisrojas514@gmail.com>
2024-03-13 17:46:56 -0400

    added basic canvas element to GameWindow component

---
+++ app/components/GameWindow.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './canvas.module.css'
+
+function GameWindow() {
+  return (
+    <canvas className={styles.frame}>
+
+    </canvas>
+  )
+}
+
+export default GameWindow
