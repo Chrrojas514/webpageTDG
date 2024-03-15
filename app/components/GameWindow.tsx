d0226477fda8cd83f70ffacaccc8d50ff63a6fa9
unknown <chrisrojas514@gmail.com>
2024-03-15 18:19:41 -0400

    renders small circle on canvas component, still learning more about canvas element and functions

--- app/components/GameWindow.tsx
+++ app/components/GameWindow.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+'use client'
+import React, {useEffect, useRef} from 'react'
 import styles from './canvas.module.css'
 
 function GameWindow() {
-  return (
-    <canvas className={styles.frame}>
+  const canvasRef = useRef(null)
 
-    </canvas>
+  const draw = (ctx: CanvasRenderingContext2D) => {
+    ctx.fillStyle = '#000000'
+    ctx.beginPath()
+    ctx.arc(50, 100, 20, 0, 2*Math.PI)
+    ctx.fill()
+  }
+  
+  useEffect( () => {
+    const canvas = canvasRef.current
+    const context = canvas.getContext('2d')
+
+    draw(context)
+  }, [draw] )
+
+  return (
+    <canvas ref={canvasRef} width='device-width' height='device-height' />
   )
 }
 
