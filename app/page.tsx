6b65910a46acc3cedea4394c7096c8b4a416d7c1
unknown <chrisrojas514@gmail.com>
2024-03-18 18:37:23 -0400

    typo

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main className={styles.main}>
       <meta charSet="utf-8"></meta>
       <title>Tower defense game?</title>
-      <h1 className={styles.gameHeader}>Learning Canvas API with react in Next.js Framework!</h1>
+      <h1 className={styles.gameHeader}>Learning Canvas API with React in Next.js Framework!</h1>
       <div className={styles.canvas_container}>
         <GameWindow />
         <ClearCanvasButton />
